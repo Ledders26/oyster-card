e08ab5b0fec767a4edb90cbb01542728153da4b9
Ledders26 <benlhall584@hotmail.co.uk>
2022-01-31 16:50:39 +0000

    Step 6 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,21 @@
 class Oystercard
 
-attr_reader :balance
+  attr_reader :balance
 
-def initialize 
-  @balance = 0
-end
+  def initialize
+    @balance = 0
+    @limit = 90
+  end
 
-def top_up(amount)
-  @balance += amount
-  @balance
-end
-  
+  def top_up(amount)
+    raise "this has exceeded the top up limit value of #{@limit}" if over_limit?(@balance, amount)
+    @balance += amount
+    @balance
+  end
+
+  private
+
+  def over_limit?(balance,amount)
+    @balance + amount > @limit
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,16 +4,20 @@ describe Oystercard do
     
 # tests if an oyster card responds to balance
 describe '#balance' do
-it 'should respond to balance' do
-
-expect(subject).to respond_to(:balance)
-end
+  it 'should respond to balance' do
+    expect(subject).to respond_to(:balance)
+  end
 end
 
 # Testing a top up function
 describe "#top_up" do
-it "should add money to the balance of the card" do
-    expect(subject.top_up(50)).to eq 50
-end
+  it "should add money to the balance of the card" do
+    expect { subject.top_up(50) }.to change { subject.balance }.by(50)
+  end
+
+  it "should stop top_up if we add something that breaks the limit" do
+    expect { subject.top_up(100) }.to raise_error("this has exceeded the top up limit value of 90")
+  end
+
 end
 end
