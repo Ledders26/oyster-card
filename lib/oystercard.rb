83de27c5a3f0ca3c93417aa04d42aff6bb194940
Haroon <haroon20@hotmail.co.uk>
2022-01-31 16:12:19 +0000

    step 5 complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,5 +5,10 @@ attr_reader :balance
 def initialize 
   @balance = 0
 end
+
+def top_up(amount)
+  @balance += amount
+  @balance
+end
   
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,6 +10,10 @@ expect(subject).to respond_to(:balance)
 end
 end
 
-
-
+# Testing a top up function
+describe "#top_up" do
+it "should add money to the balance of the card" do
+    expect(subject.top_up(50)).to eq 50
+end
+end
 end
