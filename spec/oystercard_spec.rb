556f04b82bf866327c763db2662ef5640983021b
Haroon <haroon20@hotmail.co.uk>
2022-01-31 16:02:01 +0000

    step 4 complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,14 @@ require_relative "../lib/oystercard.rb"
 
 describe Oystercard do
     
+# tests if an oyster card responds to balance
+describe '#balance' do
+it 'should respond to balance' do
+
+expect(subject).to respond_to(:balance)
+end
+end
+
 
 
 end
